0bf3220ebc6210e7dcec570e23a4c06a52e72cfa
var-username <bmt.zeroone@gmail.com>
2014-04-26 15:29:38 -0500

    Create ClientProxy.java

---
+++ xtra-materials.client/ClientProxy.java
@@ -0,0 +1,11 @@
+package xtra-materials.client;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends xtra-materials.CommonProxy{
+
+	@Override
+	public void registerRenderers(){
+		
+	}
+	
+}
