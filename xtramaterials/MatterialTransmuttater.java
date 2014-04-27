3c7e3965606db204408ab0862e2566bbf91d2242
var-username <bmt.zeroone@gmail.com>
2014-04-27 11:52:12 -0500

    Create MatterialTransmuttater.java

---
+++ xtramaterials/MatterialTransmuttater.java
@@ -0,0 +1,12 @@
+package xtramaterials;
+
+import net.minecraft.item.Item;
+
+public class MatterialTransmuttater extends Item {
+
+	public MatterialTransmuttater(int par1) {
+		super(par1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
