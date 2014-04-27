ec68b4508e49613f9e766c5d3fa210aba7ea5047
var-username <bmt.zeroone@gmail.com>
2014-04-27 11:51:56 -0500

    Create OreTransmuttater.java

---
+++ xtramaterials/OreTransmuttater.java
@@ -0,0 +1,12 @@
+package xtramaterials;
+
+import net.minecraft.item.Item;
+
+public class OreTransmuttater extends Item {
+
+	public OreTransmuttater(int par1) {
+		super(par1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
