8575a7fe6f6cd30292ec341460b3f448dad96cd4
Tim <timmussman@gmail.com>
2014-08-13 16:12:21 -0500

    Brass Sword Code and Texture
    I added in the brass sword and texture files,
    BUT I DID NOT PUT IT IN THE MAIN ITEMS FILE, OR MAKE A CRAFTING RECIPE
    FOR IT YET

---
+++ var/item/BrassSword.java
@@ -0,0 +1,12 @@
+package var.item;
+
+import net.minecraft.item.ItemSword;
+
+public class BrassSword extends ItemSword {
+
+	public SteelSword(ToolMaterial p_i45359_1_) {
+		super(p_i45359_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
